5ea38d8e1a48238aeebd344d02e0d551d2f40e10
Justin Johnson <justinaugustejohnson@gmail.com>
2025-11-30 18:53:28 -0500

    add config vars

---
+++ scripts/config.py
@@ -0,0 +1,19 @@
+# config variables
+
+# LED
+NUM_LEDS_PER_STRIP = 144
+NUM_STIPS = 3
+LED_BRIGHTNESS = 0.3
+
+# GPIO
+DATA_PIN = 18
+
+# Audio
+SAMPLE_RATE = 44100
+CHUNK_SIZE = 1024
+
+# Frequency Bands (Hz)
+    #TODO - set up frequencies
+
+# Colors
+    #TODO - set up colors
